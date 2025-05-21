f63ded91b29bf12fab024b4be5e39f986b4d607d
PedroGcruz0 <pedroestudosg@gmail.com>
2025-05-21 17:10:21 -0300

    atividade laboratorio

---
+++ P2/pratica1_poo-main/pratica1_poo-main/logica/Banco.java
@@ -0,0 +1,59 @@
+package logica;
+
+public class Banco {
+
+  private String nomeBanco;
+	private Conta[] contas;
+	private int contadorDeContas;
+	
+	public Banco(String nome) {
+		nomeBanco = nome;
+    contas = new Conta[100];
+		contadorDeContas = 0;
+	}
+
+	public int criarPupanca(){		
+		contas[contadorDeContas] = new Poupanca();
+		int aux2 = contadorDeContas;
+		contadorDeContas++;
+		return aux2;
+
+	}
+	public int criarConta(){
+		contas[contadorDeContas] = new Conta();
+		int aux = contadorDeContas;
+		contadorDeContas++;
+		return aux;
+	}
+	public int criarCorrente(){
+		contas[contadorDeContas] = new Conta();
+		int aux = contadorDeContas;
+		contadorDeContas++;
+		return aux;
+	}
+
+	public String getNome() {
+    return nomeBanco;
+  }
+
+	public boolean debitaConta(int idConta, double valor) {
+		if (idConta < contadorDeContas) {
+			return contas[idConta].debitar(valor);
+		} else {
+			return false;
+		}
+	}
+	
+	public void creditaConta(int idConta, double valor) {
+		if (idConta < contadorDeContas) {
+			contas[idConta].creditar(valor);
+		}
+	}
+
+	public double getSaldo(int idConta) {
+		if (idConta < contadorDeContas) {
+			return contas[idConta].getSaldo();
+		}
+		return -1;
+	}
+}
